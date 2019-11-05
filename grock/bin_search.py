221ca9d31d73247507ca045e2ca13ed13a9faf46
Egor Fatyushin <raccoon@sandbox-407922969>
2019-11-05 13:47:32 +0300

    grock/bin_search.py upd

--- grock/bin_search.py
+++ grock/bin_search.py
@@ -4,7 +4,7 @@
 
 def bin_search(data, value):
     lo = 0
-    hi = len(data)
+    hi = len(data) - 1
     while lo <= hi:
         mid = (lo + hi) / 2
         if data[mid] == value:
@@ -16,16 +16,96 @@ def bin_search(data, value):
     return None
 
 
-def main():
+def bin_search_recursive(data, value):
+    def recurse(lo, hi):
+        mid = (lo + hi) / 2
+        if lo > hi:
+            return None
+        elif data[mid] < value:
+            return recurse(mid + 1, hi)
+        elif data[mid] > value:
+            return recurse(lo, mid - 1)
+        else:
+            return mid
+    return recurse(0, len(data) - 1)
+
+
+def bin_search_recursive_bool(data, value):
+    if len(data) == 0:
+        return False
+
+    lo = 0
+    hi = len(data) - 1
+    mid = (lo + hi) / 2
+    if data[mid] == value:
+        return True
+    elif data[mid] < value:
+        return bin_search_recursive_bool(data[mid + 1:], value)
+    else:
+        return bin_search_recursive_bool(data[:mid], value)
+
+
+def test(f):
+    d0 = []
+    assert f(d0, -1) == None
+    assert f(d0, 1) == None
+    d1 = [1]
+    assert f(d1, -1) == None
+    assert f(d1, 1) == 0
+    assert f(d1, 2) is None
+    d2 = [1, 2]
+    assert f(d2, -1) == None
+    assert f(d2, 1) == 0
+    assert f(d2, 2) == 1
+    assert f(d2, 3) is None
     d3 = [1, 2, 3]
-    print bin_search(d3, 1)
-    print bin_search(d3, 2)
-    print bin_search(d3, 3)
+    assert f(d3, -1) == None
+    assert f(d3, 1) == 0
+    assert f(d3, 2) == 1
+    assert f(d3, 3) == 2
+    assert f(d3, 4) is None
     d4 = [1, 2, 3, 4]
-    print bin_search(d4, 1)
-    print bin_search(d4, 2)
-    print bin_search(d4, 3)
-    print bin_search(d4, 4)
+    assert f(d3, -1) == None
+    assert f(d4, 1) == 0
+    assert f(d4, 2) == 1
+    assert f(d4, 3) == 2
+    assert f(d4, 4) == 3
+    assert f(d3, 5) is None
+
+
+def test_bool(f):
+    d0 = []
+    assert f(d0, -1) == False
+    assert f(d0, 1) == False
+    d1 = [1]
+    assert f(d1, -1) == False
+    assert f(d1, 1) == True
+    assert f(d1, 2) is False
+    d2 = [1, 2]
+    assert f(d2, -1) == False
+    assert f(d2, 1) == True
+    assert f(d2, 2) == True
+    assert f(d2, 3) is False
+    d3 = [1, 2, 3]
+    assert f(d3, -1) == False
+    assert f(d3, 1) == True
+    assert f(d3, 2) == True
+    assert f(d3, 3) == True
+    assert f(d3, 4) is False
+    d4 = [1, 2, 3, 4]
+    assert f(d3, -1) == False
+    assert f(d4, 1) == True
+    assert f(d4, 2) == True
+    assert f(d4, 3) == True
+    assert f(d4, 4) == True
+    assert f(d3, 5) is False
+
+
+def main():
+    test(bin_search)
+    test(bin_search_recursive)
+    test_bool(bin_search_recursive_bool)
+    print 'OK'
 
 
 if __name__ == "__main__":
