de40f3bdf0d7cb5644ca63661c737c95439a5e81
Egor Fatyushin <raccoon@sandbox-410109003>
2019-11-27 17:51:35 +0300

    leetcode/two_sum.py fixed

--- leetcode/two_sum.py
+++ leetcode/two_sum.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-def twoSum(self, nums, target):
+def twoSum(nums, target):
         """
         :type nums: List[int]
         :type target: int
@@ -10,5 +10,20 @@ def twoSum(self, nums, target):
         """
         for i1, n1 in enumerate(nums):
             for i2, n2 in enumerate(nums):
+                if i1 == i2:
+                    continue
                 if n1 + n2 == target:
                     return i1, i2
+
+
+def test(f):
+    assert f([3, 2, 4], 6) == (1, 2)
+    print 'OK'
+
+
+def main():
+    test(twoSum)
+
+
+if __name__ == "__main__":
+    main()
