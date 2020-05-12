842068bfbf1cd31c8d06b97f55a6ea0bc4206d40
Egor Fatyushin <baphonet@gmail.com>
2020-05-12 22:25:58 +0300

    leetcode/course_schedule.py: upd

--- leetcode/course_schedule.py
+++ leetcode/course_schedule.py
@@ -19,7 +19,9 @@ class Solution(object):
 
             visited.add(course)
             for pre in courses_graph[course]:
-                return is_cycle(pre, visited)
+                print 'pre:', pre
+                if is_cycle(pre, visited):
+                    return True
 
             print 'OK, no cycle'
             return False
@@ -31,7 +33,7 @@ class Solution(object):
 
         # for course in courses_graph.iterkeys():
         for course in list(courses_graph):
-            print course    # FIXME
+            print 'course: ', course    # FIXME
             if is_cycle(course, set()):
                 return False
 
@@ -47,4 +49,6 @@ if __name__ == "__main__":
     assert not s.canFinish(2, [[1, 0], [0, 1]])
     print '\n====='
     assert not s.canFinish(4, [[2, 0], [1, 0], [3, 1], [3, 2], [1, 3]])
+    print '\n====='
+    assert s.canFinish(2, [[0, 1], [0, 2], [1, 2]])
     print 'OK'
