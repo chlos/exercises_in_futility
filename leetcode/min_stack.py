8d48a37bfd18906ac1b2ffe224f9b2317ca18bb3
Egor Fatyushin <baphonet@gmail.com>
2020-05-26 22:04:23 +0300

    leetcode/min_stack.py upd

--- leetcode/min_stack.py
+++ leetcode/min_stack.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 
-class MinStack(object):
+class MinStack_TwoStacks(object):
 
     def __init__(self):
         """
@@ -40,3 +40,44 @@ class MinStack(object):
         :rtype: int
         """
         return self.stack_min[-1]
+
+
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.data = []
+        self.min = float('inf')
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: None
+        """
+        if x <= self.min:
+            self.data.append(self.min)
+            self.min = x
+        self.data.append(x)
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        pop_element = self.data.pop()
+        if pop_element == self.min:
+            self.min = self.data.pop()
+        return pop_element
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.data[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.min
