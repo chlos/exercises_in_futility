673c9d80f2eb7205c73e5f8c7a617d4b1e7d7e11
Egor Fatyushin <egor@MacBook-Pro-Egor.local>
2020-06-30 20:29:07 +0300

    subtree_of_another_subtree: solved

--- leetcode/subtree_of_another_tree.py
+++ leetcode/subtree_of_another_tree.py
@@ -7,6 +7,56 @@ class TreeNode:
 
 
 class Solution:
+    def is_equal(self, s: TreeNode, t: TreeNode) -> bool:
+        if s is None and t is None:
+            return True
+        if s is None or t is None:
+            return False
+        return s.val == t.val and self.is_equal(s.left, t.left) and self.is_equal(s.right, t.right)
+
+    def traverse(self, s: TreeNode, t: TreeNode) -> bool:
+        if s is None:
+            return False
+        return self.is_equal(s, t) or self.traverse(s.left, t) or self.traverse(s.right, t)
+
     def isSubtree(self, s: TreeNode, t: TreeNode) -> bool:
-        # FIXME
-        pass
+        return self.traverse(s, t)
+
+
+sol = Solution()
+
+'''
+    3
+   / \
+  4   5
+ / \
+1   2
+'''
+s = TreeNode(3, TreeNode(4, TreeNode(1), TreeNode(2)), TreeNode(5))
+'''
+  4
+ / \
+1   2
+'''
+t = TreeNode(4, TreeNode(1), TreeNode(2))
+assert sol.isSubtree(s, t)
+print('OK')
+
+'''
+    3
+   / \
+  4   5
+ / \
+1   2
+   /
+  0
+'''
+s = TreeNode(3, TreeNode(4, TreeNode(1), TreeNode(2, TreeNode(0))), TreeNode(5))
+'''
+  4
+ / \
+1   2
+'''
+t = TreeNode(4, TreeNode(1), TreeNode(2))
+assert not sol.isSubtree(s, t)
+print('OK')
