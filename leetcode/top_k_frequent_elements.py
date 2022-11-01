a2f887718702f40bf37d6471df1c87c9ea3e78e0
Egor Fatyushin <enot@uber.com>
2022-11-01 23:07:42 +0200

    leetcode/top_k_frequent_elements.py upd

--- leetcode/top_k_frequent_elements.py
+++ leetcode/top_k_frequent_elements.py
@@ -4,7 +4,8 @@ import heapq
 
 
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    # heap
+    def topKFrequent_heap(self, nums: List[int], k: int) -> List[int]:
         if not nums:
             return []
 
@@ -19,3 +20,37 @@ class Solution:
             result.append(num)
 
         return result
+
+    # sorted list of pairs
+    def topKFrequent_sortedPairsList(self, nums: List[int], k: int) -> List[int]:
+        freqs = collections.defaultdict(int)
+        for n in nums:
+            freqs[n] += 1
+
+        pairs = list()
+        for n, freq in freqs.items():
+            pairs.append((n, freq))
+        pairs = sorted(pairs, key=lambda x: x[1], reverse=True)
+
+        return [pairs[i][0] for i in range(k)]
+
+    # buckets
+    # https://leetcode.com/problems/top-k-frequent-elements/solutions/81602/java-o-n-solution-bucket-sort/
+    def topKFrequent_buckets(self, nums: List[int], k: int) -> List[int]:
+        freqs = defaultdict(int)
+        for n in nums:
+            freqs[n] += 1
+
+        buckets = [[] for range in range(len(nums) + 1)]
+        for n, freq in freqs.items():
+            buckets[freq].append(n)
+
+        result = []
+        for i in range(len(nums), -1, -1):
+            bucket = buckets[i]
+            if not bucket:
+                continue
+            for n in bucket:
+                result.append(n)
+                if len(result) >= k:
+                    return result
