8974fda590a22fa9392cff39a4beaa365d3fb8e2
Egor Fatyushin <baphonet@gmail.com>
2022-12-04 22:16:33 +0200

    leetcode/merge_intervals.py: better solution

--- leetcode/merge_intervals.py
+++ leetcode/merge_intervals.py
@@ -1,9 +1,8 @@
-START = 0
-END = 1
-
-
 class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+    def merge_sort_my(self, intervals: List[List[int]]) -> List[List[int]]:
+        START = 0
+        END = 1
+
         points = []
         for start, end in intervals:
             points.append([start, START])
@@ -24,4 +23,19 @@ class Solution:
                     cur_interval[1] = point
                     result.append(cur_interval[:])
         
+        return result
+
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        START = 0
+        END = 1
+
+        intervals = sorted(intervals, key=lambda i: i[START])
+
+        result = []
+        for interval in intervals:
+            if not result or result[-1][END] < interval[START]:
+                result.append(interval)
+            else:
+                result[-1][END] = max(result[-1][END], interval[END])
+
         return result
