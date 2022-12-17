5662d12d0a1ff8d5903b9d7405239d8a1ee4129f
Egor Fatyushin <baphonet@gmail.com>
2022-12-17 19:11:51 +0200

    missed import fixed

--- leetcode/sentence_similarity_ii.py
+++ leetcode/sentence_similarity_ii.py
@@ -1,3 +1,6 @@
+import collections
+
+
 class Solution:
     def areSentencesSimilarTwo_DFS(self, sentence1: List[str], sentence2: List[str], similarPairs: List[List[str]]) -> bool:
         if len(sentence1) != len(sentence2):
