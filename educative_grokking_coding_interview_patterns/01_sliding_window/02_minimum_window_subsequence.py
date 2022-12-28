43590861914e2b151cee5d44b9056afcd31e4f88
Egor Fatyushin <baphonet@gmail.com>
2022-12-28 23:02:02 +0200

    rm debug prints

--- educative_grokking_coding_interview_patterns/01_sliding_window/02_minimum_window_subsequence.py
+++ educative_grokking_coding_interview_patterns/01_sliding_window/02_minimum_window_subsequence.py
@@ -5,30 +5,25 @@ class Solution:
     
         idx_s1, idx_s2 = 0, 0
         while idx_s1 < len(str1):
-            print('...', idx_s1, idx_s2)
             if str1[idx_s1] == str2[idx_s2]:
                 idx_s2 += 1
                 if idx_s2 == len(str2):
                     # we met the full subseq
                     # decrement idx_s2 and start reverse loop
-                    print('met full sub at idx_s1, idx_s2:', idx_s1, idx_s2)
                     start, end = idx_s1, idx_s1
                     idx_s2 -= 1
                     while idx_s2 >= 0:
                         # move idx_s2 left to check all str2 chars
                         if str1[start] == str2[idx_s2]:
-                            print(str1[start])
                             idx_s2 -= 1
                         # move start left
                         start -= 1
-                        print('start:', start, 'idx_s2:', idx_s2)
                     # move start right to the subseq start position
                     start += 1
 
                     curr_len = end - start + 1
                     if curr_len < min_len:
                         min_len = curr_len
-                        print(start, end)
                         min_subseq = str1[start:end + 1]
 
                     # we will continue checking from the next char to the first seq's char
