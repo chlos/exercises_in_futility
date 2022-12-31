238d62a3f7973622fc4a16a83043de7c328e84b9
Egor Fatyushin <baphonet@gmail.com>
2022-12-31 15:36:03 +0200

    Longest Substring Without Repeating Characters

--- educative_grokking_coding_interview_patterns/01_sliding_window/05_longest_substring_without_repeating_characters.py
+++ educative_grokking_coding_interview_patterns/01_sliding_window/05_longest_substring_without_repeating_characters.py
@@ -1,10 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# see the:
-# https://leetcode.com/problems/minimum-window-substring/discuss/26808/Here-is-a-10-line-template-that-can-solve-most-'substring'-problems
-# https://leetcode.com/problems/find-all-anagrams-in-a-string/discuss/92007/Sliding-Window-algorithm-template-to-solve-all-the-Leetcode-substring-search-problem.
-
 import collections
 
 
