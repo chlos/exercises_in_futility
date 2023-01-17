4bbe25a50f34f9623cf981d19a384b68ebf09bf6
Egor Fatyushin <baphonet@gmail.com>
2023-01-17 14:42:41 +0200

    leetcode/employee_free_time.py: pq added

--- leetcode/employee_free_time.py
+++ leetcode/employee_free_time.py
@@ -1,10 +1,15 @@
+import heapq
+
+
 # Definition for an Interval.
 class Interval:
     def __init__(self, start: int = None, end: int = None):
         self.start = start
         self.end = end
 
+
 class Solution:
+    # sweep line
     def employeeFreeTime(self, schedule: '[[Interval]]') -> '[Interval]':
         free_intervals = []
 
@@ -22,5 +27,24 @@ class Solution:
             
             if curr_latest_end is None or work_interval.end > curr_latest_end:
                 curr_latest_end = work_interval.end
+
+        return free_intervals
+
+    # min heap / priority queue
+    def employeeFreeTime_pq(self, schedule: '[[Interval]]') -> '[Interval]':
+        free_intervals = []
+
+        pq_work_intervals = []
+        for user_work_intervals in schedule:
+            for user_work_interval in user_work_intervals:
+                start, end = user_work_interval.start, user_work_interval.end
+                heapq.heappush(pq_work_intervals, (start, end))
+
+        start, curr_latest_end = heapq.heappop(pq_work_intervals)
+        while pq_work_intervals:
+            start, end = heapq.heappop(pq_work_intervals)
+            if start > curr_latest_end:
+                free_intervals.append(Interval(curr_latest_end, start))
+            curr_latest_end = max(curr_latest_end, end)
 
         return free_intervals
