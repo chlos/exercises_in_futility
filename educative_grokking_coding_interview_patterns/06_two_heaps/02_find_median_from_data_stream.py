dca53ad39b38a8c28a4931c5dfc375b977bc4be8
Egor Fatyushin <baphonet@gmail.com>
2023-01-30 23:27:44 +0200

    leetcode/find_median_from_data_stream.py

---
+++ educative_grokking_coding_interview_patterns/06_two_heaps/02_find_median_from_data_stream.py
@@ -0,0 +1,38 @@
+from min_heap import *
+from max_heap import *
+import heapq
+
+# Tip: You may use some of the code templates provided
+# in the support files
+
+class MedianOfStream:
+  def __init__(self):
+      # elements less than median; max heap
+      self.small_heap = []
+      # elements greater than median; min heap
+      self.large_heap = []
+
+  def insert_num(self, num: int) -> None:
+      if len(self.small_heap) == 0 and len(self.large_heap) == 0:
+          self.small_heap.append(-num)
+          return
+
+      if num < self.find_median():
+          heapq.heappush(self.small_heap, -num)
+      else:
+          heapq.heappush(self.large_heap, num)
+
+      # re-balance if needed
+      if len(self.small_heap) < len(self.large_heap):
+          tmp_n = heapq.heappop(self.large_heap)
+          heapq.heappush(self.small_heap, -tmp_n)
+      elif len(self.small_heap) - len(self.large_heap) > 1:
+          tmp_n = -heapq.heappop(self.small_heap)
+          heapq.heappush(self.large_heap, tmp_n)
+
+  def find_median(self) -> float:
+      # even number of elements => median is an avg of two mid values
+      if (len(self.small_heap) + len(self.large_heap)) % 2 == 0:
+          return (-self.small_heap[0] + self.large_heap[0]) / 2.0
+      # otherwise it's mid values
+      return -self.small_heap[0]
