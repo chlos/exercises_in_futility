e2f99320665d128a1344b908f8122d5d94bf28fb
Egor Fatyushin <baphonet@gmail.com>
2023-04-11 15:40:18 +0300

    find_max_knapsack_profit: 1d version added

--- educative_grokking_coding_interview_patterns/13_dynamic_programming/01_0_1_knapsack.py
+++ educative_grokking_coding_interview_patterns/13_dynamic_programming/01_0_1_knapsack.py
@@ -1,5 +1,5 @@
 # https://leetcode.com/discuss/study-guide/1152328/01-Knapsack-Problem-and-Dynamic-Programming
-def find_max_knapsack_profit(capacity, weights, values):
+def find_max_knapsack_profit_2d(capacity, weights, values):
     # dp matrix:
     #         cap=0   cap=1   cap=2 ...
     # weight=0
@@ -25,3 +25,20 @@ def find_max_knapsack_profit(capacity, weights, values):
                 dp[weight_row + 1][curr_cap] =  dp[weight_row][curr_cap]
 
     return dp[-1][-1]
+
+
+def find_max_knapsack_profit(capacity, weights, values):
+    dp = [0 for _ in range(capacity + 1)]
+
+    for weight_row in range(len(weights)):
+        for curr_cap in range(capacity, 0, -1):
+            # this item could fit the knapsack
+            if weights[weight_row] <= curr_cap:
+                dp[curr_cap] = max(
+                    values[weight_row] + dp[curr_cap - weights[weight_row]],
+                    dp[curr_cap]
+                )
+            else:
+                break
+
+    return dp[-1]
