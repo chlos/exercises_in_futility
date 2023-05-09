f6d45c11c9f26d35f55685de65bc01f6e0fbc4e8
Egor Fatyushin <baphonet@gmail.com>
2023-05-09 12:21:49 +0300

    format

--- educative_grokking_coding_interview_patterns/15_topological_sort/01_compilation_order.py
+++ educative_grokking_coding_interview_patterns/15_topological_sort/01_compilation_order.py
@@ -1,32 +1,30 @@
 from collections import deque, defaultdict
 
-# Tip: You may use some of the code templates provided
-# in the support file
 
 def find_compilation_order(dependencies):
-  sorted_order = []
+    sorted_order = []
 
-  # build a graph and incoming degrees counter
-  graph = defaultdict(list)
-  in_degrees = defaultdict(int)
-  for child, parent in dependencies:
-    graph[parent].append(child)
-    in_degrees[child] += 1
+    # build a graph and incoming degrees counter
+    graph = defaultdict(list)
+    in_degrees = defaultdict(int)
+    for child, parent in dependencies:
+        graph[parent].append(child)
+        in_degrees[child] += 1
 
-  # add sources to the queue
-  # source: any vertex with no incoming edge and only outgoing edges
-  sources = deque([])
-  for node in graph:
-    if in_degrees[node] == 0:
-      sources.append(node)
+    # add sources to the queue
+    # source: any vertex with no incoming edge and only outgoing edges
+    sources = deque([])
+    for node in graph:
+        if in_degrees[node] == 0:
+            sources.append(node)
 
-  # build sorted order list using only sources
-  while sources:
-    source_node = sources.popleft()
-    sorted_order.append(source_node)
-    for child_node in graph[source_node]:
-      in_degrees[child_node] -= 1
-      if in_degrees[child_node] == 0:
-        sources.append(child_node)
+    # build sorted order list using only sources
+    while sources:
+        source_node = sources.popleft()
+        sorted_order.append(source_node)
+        for child_node in graph[source_node]:
+            in_degrees[child_node] -= 1
+            if in_degrees[child_node] == 0:
+                sources.append(child_node)
 
-  return sorted_order
+    return sorted_order
