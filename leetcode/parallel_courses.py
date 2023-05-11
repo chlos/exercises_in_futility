81610d97a206f8376e95cb1a4f49e7c0efe10983
Egor Fatyushin <baphonet@gmail.com>
2023-05-11 18:23:02 +0300

    leetcode/parallel_courses.py: upd: dfs added

--- leetcode/parallel_courses.py
+++ leetcode/parallel_courses.py
@@ -3,7 +3,7 @@ import collections
 
 class Solution:
     # topological sort; Kahn's algorithm; BFS
-    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+    def minimumSemesters_topoSort(self, n: int, relations: List[List[int]]) -> int:
         # init incoming degrees counter
         # in some reason N could be > than len(all_courses_set) !!!???
         # so we need to init [1...N] courses
@@ -50,5 +50,67 @@ class Solution:
         # check if there are unreachable courses
         if len(courses_taken) != n:
             return -1
+
+        return num_semesters
+
+    # dfs; cycle detection; path length calculation
+    def minimumSemesters(self, n: int, relations: List[List[int]]) -> int:
+        # build graph
+        graph = collections.defaultdict(list)
+        for parent_node, child_node in relations:
+            graph[parent_node].append(child_node)
+
+        # check cycles - naive, timeout
+        # visited = [False] * (n + 1)
+        # def has_cycle_dfs(node):
+        #     if visited[node]:
+        #         return True
+        #     visited[node] = True
+        #     for child_node in graph[node]:
+        #         if has_cycle_dfs(child_node):
+        #             return True
+        #     visited[node] = False
+        #     return False
+
+        # check cycles
+        visited = {}
+        IN_PROGRESS, VISITED = 1, 0
+
+        def has_cycle_dfs(node: int):
+            # return True if graph has a cycle
+            if node in visited:
+                return visited[node]
+            else:
+                # mark as visiting
+                visited[node] = IN_PROGRESS
+            for end_node in graph[node]:
+                if has_cycle_dfs(end_node):
+                    # we meet a cycle!
+                    return True
+            # mark as visited
+            visited[node] = VISITED
+            return False
+
+        for node in range(1, n + 1):
+            if has_cycle_dfs(node):
+                return -1
+
+        # calculate the longest path
+        visited_len = {}
+
+        def calc_path_len_dfs(node):
+            if node in visited_len:
+                return visited_len[node]
+
+            max_len = 1
+            for child_node in graph[node]:
+                max_len = max(max_len, calc_path_len_dfs(child_node) + 1)
+            visited_len[node] = max_len
+
+            return max_len
+
+        num_semesters = 0
+        for node in range(1, n + 1):
+            num_semesters = max(num_semesters, calc_path_len_dfs(node))
 
         return num_semesters
